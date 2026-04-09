ed869c51e7dd5e13fe1613745c99039986e6d70f
RAJ KUMAR PANDEY <pandeyg.raj@gmail.com>
2026-04-08 22:12:56 -0400

    Update run_case1VScase2.sh

--- run_case1VScase2.sh
+++ run_case1VScase2.sh
@@ -20,7 +20,17 @@ read THREADS
 OUT_DIR=case1Vscase2
 mkdir -p "$OUT_DIR"
 
+breakdownresult="Case1vsCase2_${EXP_LABEL}_summary.txt"
+touch "$breakdownresult"
+
 RAW_FILE="${OUT_DIR}/${EXP_LABEL}_Load${FIELD_LENGTH}Bytes_run.scr"
+
+# reset case1 vs case2 statistic before load 
+ssh rzp5412@10.10.1.2 "/mydata/cassandra/bin/nodetool ecwritestats --reset" 
+ssh rzp5412@10.10.1.3 "/mydata/cassandra/bin/nodetool ecwritestats --reset" 
+ssh rzp5412@10.10.1.4 "/mydata/cassandra/bin/nodetool ecwritestats --reset" 
+ssh rzp5412@10.10.1.5 "/mydata/cassandra/bin/nodetool ecwritestats --reset" 
+ssh rzp5412@10.10.1.6 "/mydata/cassandra/bin/nodetool ecwritestats --reset" 
 
 # Load phase once
 echo "Load phase: Loading $RECORD_COUNT records of size ${FIELD_LENGTH} bytes"
@@ -33,4 +43,11 @@ $YCSB_DIR load $DB -threads $WTHREADS \
 
 echo "Load phase: Done $RECORD_COUNT records of size ${FIELD_LENGTH} bytes"
 
+#collect case1 vs case 2 statistic from all nodes and sava in a file 
+#Load done collect data case1 vs case2
+ssh rzp5412@10.10.1.2 "/mydata/cassandra/bin/nodetool ecwritestats" >> "$breakdownresult"
+ssh rzp5412@10.10.1.3 "/mydata/cassandra/bin/nodetool ecwritestats" >> "$breakdownresult"
+ssh rzp5412@10.10.1.4 "/mydata/cassandra/bin/nodetool ecwritestats" >> "$breakdownresult"
+ssh rzp5412@10.10.1.5 "/mydata/cassandra/bin/nodetool ecwritestats" >> "$breakdownresult"
+ssh rzp5412@10.10.1.6 "/mydata/cassandra/bin/nodetool ecwritestats" >> "$breakdownresult"
 

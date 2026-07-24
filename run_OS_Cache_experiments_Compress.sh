d9c62d4a78058f40ba3695bc6e2e52195d3932ed
RAJ KUMAR PANDEY <pandeyg.raj@gmail.com>
2026-07-24 11:24:01 -0400

    Update run_OS_Cache_experiments_Compress.sh

--- run_OS_Cache_experiments_Compress.sh
+++ run_OS_Cache_experiments_Compress.sh
@@ -324,7 +324,8 @@ read THREADS
 #     delete_snapshot
 #   Between on/off: hard_restart of next iteration wipes everything clean
 # =============================================================================
-for COMPRESSION in "on" "off"; do
+#for COMPRESSION in "on" "off"; do
+for COMPRESSION in "on"; do
 
     # Set correct binary for this compression state
     if echo "$EXP_LABEL" | grep -qi "rep"; then
